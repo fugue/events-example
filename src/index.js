e1e1f3f62e84e09e8f609800b502ff55771153f1
Curtis Myzie <curtis@fugue.co>
2020-09-15 20:58:50 -0400

    Various fixes; eslint

--- src/index.js
+++ src/index.js
@@ -1,137 +1,176 @@
-/* 
-  Go to your Fugue Tenant Dashboard and create an API Client.
-  Note your CLIENT_ID and your CLIENT_SECRET at the bottom of this screen,
-  input those values as Environment Variables.
+/* eslint-disable no-await-in-loop, no-constant-condition */
+const aws = require('aws-sdk');
+const https = require('https');
+const splunk = require('splunk-logging');
+const bunyan = require('bunyan');
 
-  This function will extract the details of your Fugue Event
-  (Compliance, Drift, Remediation) and log them to any 3rd party tool that you
-  may be using to generate reports.  The message is set as variable called
-  "body".  This will be your output parameter to be sent to any 3rd party tool. 
-*/
+const log = bunyan.createLogger({ name: 'fugue-events' });
 
-const aws = require('aws-sdk')
-const https = require('https')
-const splunk = require('splunk-logging')
+let secretValue = null;
 
-var secretValue = null
-
-async function getSecret (secretName, region) {
-  var client = new aws.SecretsManager({ region: region })
+async function getSecret(secretName, region) {
+  const client = new aws.SecretsManager({ region });
   return new Promise((resolve, reject) => {
-    client.getSecretValue({ SecretId: secretName }, function (err, data) {
+    client.getSecretValue({ SecretId: secretName }, (err, data) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
-        resolve(JSON.parse(data.SecretString))
+        resolve(JSON.parse(data.SecretString));
       }
-    })
-  })
+    });
+  });
 }
 
-async function getEvents (apiUrl, clientId, clientSecret, offset) {
+async function getEvents(apiUrl, clientId, clientSecret, offset) {
   const options = {
     method: 'GET',
-    auth: clientId + ':' + clientSecret
-  }
+    auth: `${clientId}:${clientSecret}`,
+  };
+  let getUrl = apiUrl;
   if (offset) {
-    apiUrl += '?offset=' + offset
+    getUrl += `?offset=${offset}`;
   }
   return new Promise((resolve, reject) => {
-    const req = https.get(apiUrl, options, res => {
-      let body = ''
-      res.on('data', chunk => {
-        body += chunk
-      })
+    const req = https.get(getUrl, options, (res) => {
+      let body = '';
+      res.on('data', (chunk) => {
+        body += chunk;
+      });
       res.on('end', () => {
-        let json = JSON.parse(body)
-        if (res.statusCode != 200) {
-          reject(new Error('ERROR ' + res.statusCode + ': ' + json.message))
+        const json = JSON.parse(body);
+        if (res.statusCode !== 200) {
+          reject(new Error(`Fugue API returned ${res.statusCode}: ${json.message}`));
         } else {
-          resolve(json)
+          resolve(json);
         }
-      })
-    })
-    req.on('error', error => {
-      reject(error)
-    })
-  })
+      });
+    });
+    req.on('error', (error) => {
+      reject(error);
+    });
+  });
 }
 
-exports.handler = async function (event, context) {
+async function sendToSplunk(splunkLogger, payload) {
+  return new Promise((resolve, reject) => {
+    splunkLogger.send(payload, (err, resp, body) => {
+      if (err) {
+        reject(new Error(`Error sending to Splunk: ${err}`));
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
 
-  var message;
-  const rawMessage = event.Records[0].Sns.Message
-  try {
-    message = JSON.parse(rawMessage)
-  } catch (err) {
-    console.log('Ignoring non-JSON message:', rawMessage)
-    return
-  }
-
-  const secretArn = process.env.SECRET_ARN
+async function getConfiguration() {
+  // Retrieve secret ARN from the environment
+  const secretArn = process.env.SECRET_ARN;
   if (!secretArn) {
-    throw Error('SECRET_ARN is unset')
+    log.error('SECRET_ARN is unset');
+    throw Error('SECRET_ARN is unset');
   }
-
-  const splunkUrl = process.env.SPLUNK_URL
+  // Retrieve Splunk publishing URL from the environment
+  const splunkUrl = process.env.SPLUNK_URL;
   if (!splunkUrl) {
-    throw Error('SPLUNK_URL is unset')
+    log.error('SPLUNK_URL is unset');
+    throw Error('SPLUNK_URL is unset');
   }
-
+  // Retrieve the secret itself
   if (!secretValue) {
-    secretValue = await getSecret(secretArn)
+    secretValue = await getSecret(secretArn);
   }
-  const fugueId = secretValue.FUGUE_API_ID
-  const fugueSecret = secretValue.FUGUE_API_SECRET
-  const splunkToken = secretValue.SPLUNK_TOKEN
-  const apiUrl = message.api_url
-
-  if (!fugueId || !fugueSecret) {
-    throw Error('Fugue API credentials not found in secret')
+  // Extract the Fugue API credentials and the Splunk API token from the secret
+  const fugueId = secretValue.FUGUE_API_ID;
+  const fugueSecret = secretValue.FUGUE_API_SECRET;
+  const splunkToken = secretValue.SPLUNK_TOKEN;
+  if (!fugueId) {
+    log.error('FUGUE_API_ID is not in the secret');
+    throw Error('FUGUE_API_ID is not in the secret');
+  }
+  if (!fugueSecret) {
+    log.error('FUGUE_API_SECRET is not in the secret');
+    throw Error('FUGUE_API_SECRET is not in the secret');
   }
   if (!splunkToken) {
-    throw Error('SPLUNK_TOKEN not found in secret')
+    log.error('SPLUNK_TOKEN is not in the secret');
+    throw Error('SPLUNK_TOKEN is not in the secret');
+  }
+  return {
+    splunkUrl, splunkToken, fugueId, fugueSecret,
+  };
+}
+
+// eslint-disable-next-line func-names, no-unused-vars
+exports.handler = async function (event, context) {
+  let message;
+  const rawMessage = event.Records[0].Sns.Message;
+  try {
+    message = JSON.parse(rawMessage);
+  } catch (err) {
+    log.warn('Ignoring non-JSON message:', rawMessage);
+    return 0;
   }
 
-  console.log('Requesting events:', apiUrl)
+  // Extract key information from the Fugue notification
+  const apiUrl = message.api_url;
+  const scanId = message.scan_id;
+  const environmentId = message.environment_id;
+  const environmentName = message.environment_name;
+  const { summary } = message;
 
-  var offset = 0;
-  var events = [];
+  // Get API credentials and other configuration
+  const conf = await getConfiguration();
 
   // Each request will retrieve up to 100 events. Loop as needed to retrieve
   // all events by updating the `offset` query parameter as needed.
+  let offset = 0;
+  const events = [];
   while (true) {
-    const response = await getEvents(apiUrl, fugueId, fugueSecret, offset)
-    events.push(...response.items)
+    const response = await getEvents(apiUrl, conf.fugueId, conf.fugueSecret, offset);
+    events.push(...response.items);
     if (!response.is_truncated) {
-      break
+      break;
     }
-    offset = response.next_offset
+    offset = response.next_offset;
   }
 
-  var SplunkLogger = new splunk.Logger({
-    token: splunkToken,
-    url: splunkUrl
-  })
+  log.info({
+    count: events.length,
+    scan_id: scanId,
+    environment_id: environmentId,
+    environment_name: environmentName,
+    summary,
+  }, 'Retrieved events from Fugue');
 
-  console.log('Retrieved', events.length, 'events')
+  // Create Splunk logger with configured URL and token
+  const splunkLogger = new splunk.Logger({
+    token: conf.splunkToken,
+    url: conf.splunkUrl,
+    maxBatchCount: 1,
+  });
 
-  for (let i = 0; i < events.length; i++) {
-    console.log('Event:', events[i])
-    let splunkPayload = { message: events[i] }
-    SplunkLogger.send(splunkPayload, function(err, resp, body) {
-      if (err) {
-        throw Error('Splunk error: ' + err)
-      }
-    })
+  log.info({ splunk_url: conf.splunkUrl }, 'Sending events to Splunk');
+
+  // Loop to send each event and some metadata to Splunk
+  for (let i = 0; i < events.length; i += 1) {
+    const fugueEvent = events[i];
+    const response = await sendToSplunk(splunkLogger, {
+      message: {
+        event: fugueEvent,
+        scan_id: scanId,
+        environment_id: environmentId,
+        environment_name: environmentName,
+        summary,
+      },
+    });
+    log.info({
+      event_id: fugueEvent.id,
+      event_type: fugueEvent.event_type,
+      index: i,
+      response,
+    }, 'Event sent to Splunk');
   }
 
-  SplunkLogger.flush(function(err, resp, body) {
-    console.log("Response from Splunk:", err, resp, body);
-    if (err) {
-      throw Error('Splunk error: ' + err)
-    }
-  })
-
-  return events.length
-}
+  return events.length;
+};
